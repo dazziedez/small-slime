3be2db5113a7b733762e40e40e5d5bd8009a0793
dazzie2load <79106393+dazziedez@users.noreply.github.com>
2024-02-14 15:10:46 +0000

    added brief descriptiom

--- cogs/help.py
+++ cogs/help.py
@@ -6,7 +6,7 @@ import discord
 
 class CustomHelpCommand(HelpCommand):
     def __init__(self):
-        super().__init__(command_attrs={'aliases': ['h', 'commands']})
+        super().__init__(command_attrs={'aliases': ['h', 'commands'], 'brief': 'hi'})
 
     async def send_bot_help(self, mapping):
         embeds = [await self.create_cog_embed(cog, commands) for cog, commands in mapping.items() if commands]
@@ -33,8 +33,8 @@ class CustomHelpCommand(HelpCommand):
                     command.aliases) if command.aliases else "No aliases"
 
                 usage = self.get_command_signature(command)
-                embed.add_field(name="Aliases", value=aliases, inline=False)
-
+                embed.add_field(name="Aliases", value=aliases, inline=True)
+                embed.add_field(name="Info", value=command.brief or "Nop", inline=True)
                 embed.add_field(
                     name="Usage", value=f"```{usage}```", inline=False)
 
@@ -54,7 +54,7 @@ class CustomHelpCommand(HelpCommand):
                 group.aliases) if group.aliases else "No aliases", inline=True)
 
             embed.add_field(
-                name="Module", value=group.cog.qualified_name, inline=True)
+                name="Info", value=group.brief or "Nop", inline=True)
 
             embed.add_field(
                 name="Usage", value=f"```{self.get_command_signature(group)}```", inline=False)
@@ -69,7 +69,7 @@ class CustomHelpCommand(HelpCommand):
                 command.aliases) if command.aliases else "No aliases", inline=True)
 
             embed.add_field(
-                name="Module", value=group.cog.qualified_name, inline=True)
+                name="Info", value=command.brief or "Nop", inline=True)
 
             embed.add_field(
                 name="Usage", value=f"```{self.get_command_signature(command)}```", inline=False)
@@ -86,7 +86,7 @@ class CustomHelpCommand(HelpCommand):
         usage = f"```{self.get_command_signature(command)}```"
         embed.add_field(name="Aliases", value=aliases, inline=True)
         embed.add_field(
-            name="Module", value=command.cog.qualified_name if command.cog else "Not in a module", inline=True)
+            name="Info", value=command.brief or "Nop", inline=True)
         embed.add_field(name="Usage", value=usage, inline=False)
 
         await self.context.reply(embed=embed)
