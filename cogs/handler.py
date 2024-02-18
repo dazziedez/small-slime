6b260309de85f7433781813b93ee33420817663e
dazzie2load <79106393+dazziedez@users.noreply.github.com>
2024-02-18 15:22:32 +0000

    Update handler.py

--- cogs/handler.py
+++ cogs/handler.py
@@ -5,40 +5,44 @@ from fuzzywuzzy import process
 import sys
 import traceback
 
+from rich import print
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from utils import slime
 
+
 class ErrorHandler(commands.Cog):
     bot: 'slime.Bot'
-    
+
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send_embed(description='You are missing a required argument.')
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send_embed(description='Invalid argument provided.')
-        elif isinstance(error, commands.CheckFailure) or isinstance(error, commands.MissingPermissions):
-            await ctx.send_embed(description='You do not have permission to use this command.')
-        elif isinstance(error, commands.BotMissingPermissions):
-            await ctx.send_embed(description='I do not have the required permissions to run this command.')
-           
-        elif isinstance(error, commands.CommandNotFound):
-            commands_list = [cmd.name for cmd in self.bot.commands]
-            closest_match, score = process.extractOne(
-                ctx.invoked_with, commands_list)
-            if score > 50:
-                await ctx.send_embed(description=f"Command not found. Did you mean `{closest_match}`?")
-        else:
-            await ctx.send_embed(description=error)
+        match error:
+            case isinstance(error, commands.MissingRequiredArgument):
+                await ctx.send_embed(description='You are missing a required argument.')
+            case isinstance(error, commands.BadArgument):
+                await ctx.send_embed(description='Invalid argument provided.')
+            case isinstance(error, commands.CheckFailure):
+                await ctx.send_embed(description='You are not allowed to use this command.')
+            case isinstance(error, commands.MissingPermissions):
+                await ctx.send_embed(description='You do not have permission to use this command.')
+            case isinstance(error, commands.BotMissingPermissions):
+                await ctx.send_embed(description='I do not have the required permissions to run this command.')
+            case isinstance(error, commands.CommandNotFound):
+                commands_list = [cmd.name for cmd in self.bot.commands]
+                closest_match, score = process.extractOne(
+                    ctx.invoked_with, commands_list)
+                if score > 50:
+                    await ctx.send_embed(description=f"Command not found. Did you mean `{closest_match}`?")
+            case _:
+                await ctx.send_embed(description=error)
 
-        print(
-            f'[Error   ] Ignoring exception in command {ctx.command}:', file=sys.stderr)
+        print(f'[red1][Error   ][/red1] Ignoring exception in command {ctx.command}:', file=sys.stderr)
         traceback.print_exception(
             type(error), error, error.__traceback__, file=sys.stderr)
 
