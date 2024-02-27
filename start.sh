138668722406cda1088ae677f119d843e49dae30
dazzie2load <79106393+dazziedez@users.noreply.github.com>
2024-02-27 20:33:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,8 +13,29 @@ fi
 echo "Activating virtual environment..."
 source $VENV_DIR/Scripts/activate
 
-echo "Installing requirements from $REQUIREMENTS_FILE..."
-pip install -r $REQUIREMENTS_FILE
+echo "Installing/upgrading requirements from $REQUIREMENTS_FILE..."
+
+handle_version_mismatch() {
+    local package_name=$1
+    local installed_version=$(pip show $package_name | grep Version | awk '{print $2}')
+    local required_version=$(grep "$package_name" $REQUIREMENTS_FILE | cut -d'=' -f3)
+
+    if [ "$installed_version" == "$required_version" ]; then
+        echo "Version matches for $package_name: $installed_version"
+    elif [ "$(printf '%s\n' "$installed_version" "$required_version" | sort -V | head -n1)" == "$installed_version" ]; then
+        echo "Installed version of $package_name ($installed_version) is newer. Uninstalling and reinstalling..."
+        pip uninstall -y $package_name
+        pip install $package_name
+    else
+        echo "Installed version of $package_name ($installed_version) is older. Upgrading..."
+        pip install --upgrade $package_name
+    fi
+}
+
+while IFS= read -r line; do
+    package=$(echo $line | cut -d'=' -f1)
+    handle_version_mismatch $package
+done < $REQUIREMENTS_FILE
 
 echo "Starting $MAIN_SCRIPT..."
 python $MAIN_SCRIPT tee /dev/tty
